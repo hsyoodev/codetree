f2173f40458c060245b3ba187e7bcd191bf5253e
HOSEON YOO <hsyoodev@icloud.com>
2026-04-21 23:57:42 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260421/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in)
+    }
+
+    public static void printRect(int n, int m) {
+
+    }
+}
